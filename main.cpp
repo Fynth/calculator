03da7a228bf0d5900bc616faeaf820a9e10f6a5a
Fynth <umenyasvoi@gmail.com>
2021-06-02 12:51:07 +0300

    added full ui

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("Банковский калькулятор");
     w.show();
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,21 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui -> creditTable -> setRowCount(10);
+    ui -> creditTable_2 -> setRowCount(10);
+    ui -> creditTable_3 -> setRowCount(10);
+    ui -> creditTable -> horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui -> creditTable_2 -> horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui -> creditTable_3 -> horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
 }
 
